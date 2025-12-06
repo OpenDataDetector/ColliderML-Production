fb8b05f409d1609bde4401c8ee1d028ba6bd6616
Daniel Murnane <murnanedaniel@hotmail.com>
2025-12-06 04:57:55 -0800

    Auto-commit for campaign 'full_pileup', dataset 'jets', version 'v1'

---
+++ scripts/dataset/slurm_upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account             m4958
+#SBATCH --constraint          cpu
+#SBATCH --cpus-per-task       256
+#SBATCH --error               /global/cfs/cdirs/m4958/usr/danieltm/ColliderML/software/colliderml_dev/scripts/dataset/logs/job_0_%j.err
+#SBATCH --job-name            huggingface_upload
+#SBATCH --nodes               1
+#SBATCH --ntasks-per-node     1
+#SBATCH --output              /global/cfs/cdirs/m4958/usr/danieltm/ColliderML/software/colliderml_dev/scripts/dataset/logs/job_0_%j.out
+#SBATCH --qos                 debug
+#SBATCH --time                0:30:00
+
+cd /global/cfs/cdirs/m4958/usr/danieltm/ColliderML/software/colliderml_dev/scripts/dataset
+conda activate collider-env
+
+python upload_to_huggingface.py unified_dataset_config.yaml
